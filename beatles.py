dcd2ed8fd05b66405232d82370fa63b0f80a0f01
Nichole Denga <161051399+ayenichole@users.noreply.github.com>
2025-09-26 01:54:33 +0200

    Add files via upload

---
+++ beatles.py
@@ -0,0 +1,30 @@
+# step 1
+beatles = []
+print("Step 1:", beatles)
+
+# step 2
+beatles.append("John Lennon")
+beatles.append("Paul McCartney")
+beatles.append("George Harrison")
+print("Step 2:", beatles)
+new_members = 0
+# step 3
+for i in range(2):
+ new_members = input("Enter new members:")
+ beatles.append(new_members)
+print("Step 3:", beatles)
+
+
+# step 4
+del beatles[-1]
+del beatles[-1]
+print("Step 4:", beatles)
+
+# step 5
+beatles.insert(0,"Ringo Starr")
+print("Step 5:", beatles)
+
+
+# testing list legth
+print("The Fab", len(beatles))
+
